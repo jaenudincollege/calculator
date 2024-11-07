8c57b82fb46c8478eac481213dce480dbefe9a43
Jaenudin Firdaus <jaenudinfirdaus86@gmail.com>
2024-11-07 09:03:26 +0700

    change query selector and logic

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const buttons = document.querySelectorAll("button");
-const input = document.querySelector("input");
-
-let value = [];
+const display = document.querySelector(".display");
 
 for (const btn of buttons) {
   btn.addEventListener("click", (e) => {
-    
+    const input = btn.textContent;
+    if (display.textContent === "0") display.textContent = input;
+    else display.textContent += input;
   });
 }
