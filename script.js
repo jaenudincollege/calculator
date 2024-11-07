d1c79edcba25e04523253bdcb99e94726777ba7e
Jaenudin Firdaus <jaenudinfirdaus86@gmail.com>
2024-11-07 08:36:43 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll("button");
+const input = document.querySelector("input");
+
+let value = [];
+
+for (const btn of buttons) {
+  btn.addEventListener("click", (e) => {
+    
+  });
+}
