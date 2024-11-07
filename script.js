8d752057bed15daaf9e5f1eea4a4e654497af085
Jaenudin Firdaus <jaenudinfirdaus86@gmail.com>
2024-11-07 09:16:31 +0700

    Add delete and clear logic

--- script.js
+++ script.js
@@ -6,5 +6,9 @@ for (const btn of buttons) {
     const input = btn.textContent;
     if (display.textContent === "0") display.textContent = input;
     else display.textContent += input;
+
+    if (btn.textContent === "AC") display.textContent = 0;
+    else if (btn.textContent === "DEL")
+      display.textContent = display.textContent.slice(0, -4);
   });
 }
